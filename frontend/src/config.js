d69a5e27462dea92a7d3f546d73c771e6cec4846
iamkartik0704 <iamkartik0704@gmail.com>
2026-05-31 18:08:55 +0530

    Fixing url 1

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 // This automatically switches between your local dev URL and the live URL
 export const API_BASE = process.env.NODE_ENV === 'production' 
-  ? 'https://rankmatrix.onrender.com/' 
+  ? 'https://rankmatrix.onrender.com' 
   : 'http://localhost:5000';
