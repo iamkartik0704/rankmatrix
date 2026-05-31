e42e5f1b83a3e3eb43f6828b962570d14ade7ac2
iamkartik0704 <iamkartik0704@gmail.com>
2026-05-31 17:42:50 +0530

    Fix catch-all route regex

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/build')));
 
   // For any route that isn't /api, send index.html
-  app.get('*', (req, res) => {
+  app.get('.*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
   });
 }
